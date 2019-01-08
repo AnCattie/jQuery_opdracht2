a6b452fe1aff8e10847e3713b3f51213c0e4c980
An <an.arletta.cattie@gmail.com>
2019-01-08 09:08:46 +0100

    no message

--- Opdracht_7/main.js
+++ Opdracht_7/main.js
@@ -1,12 +1,13 @@
+// Other methode to initialise jQuery object.
+// $(function(){
 $(document).ready(function(){
     /* On click: Enlarge the font (120%) */
         $("#instructions").children().first().click(function(){
-            $("#content").css("font-size","120%")
+            $("#content").css("font-size","120%");
         });
-    
     /* On click: Decrease the font size (80%) */
         $("ol li:nth-child(2)").click(function(){
-            $("#content").css("font-size","80%")
+            $("#content").css("font-size","80%");
         });
     /* On click: Bold or normalize the green words */
         $("ol li:nth-child(3)").click(function(){
@@ -24,15 +25,15 @@ $(document).ready(function(){
         });
     /* On click: Replace the logo with another image */
         $("ol li:nth-child(5)").hover(function(){
-            $("img").attr("src","./images/koala.JPG")
+            $("img").attr("src","./images/koala.JPG");
         });
     /* On hover: Display the URL of links in a tooltip when hovering over links */
         $("a").hover(function(){
-            $(this).prop("title", $(this).attr("href"))
+            $(this).prop("title", $(this).attr("href"));
         });
     /* On click: add "Chapter 1:" before the 1st title h2 and "Chapter 2:" before the 2nd title h2 */
-        $("ol:last-child:first").click(function(){
-            $("h2:first").before().text("Chapter 1:")
+        $("ol:last-child").click(function(){
+            $("h2:first").prepend("Chapter 1: ");
+            $("h2:eq(1)").prepend("Chapter 2: ");
         });
-        
 });
